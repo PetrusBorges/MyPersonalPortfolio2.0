bb39ebad618280cc51d164416062f77ca683cd8e
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-04-25 11:22:09 -0300

    fix: styles on Opacity component

--- app/components/Opacity/index.tsx
+++ app/components/Opacity/index.tsx
@@ -2,8 +2,6 @@ const Opacity = () => {
   return (
     <svg
       className="animated-fade-in-slow"
-      width="2400"
-      height="1350"
       viewBox="0 0 2400 1350"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
