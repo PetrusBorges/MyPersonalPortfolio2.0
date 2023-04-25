844e0077c0c8cece76dda75337e946fce46b8d23
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-04-24 22:48:32 -0300

    feat: add component Opacity

---
+++ app/components/Opacity/index.tsx
@@ -0,0 +1,16 @@
+const Opacity = () => {
+  return (
+    <svg
+      className="animated-fade-in-slow"
+      width="2400"
+      height="1350"
+      viewBox="0 0 2400 1350"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="2400" height="1350" fill="black"/>
+    </svg>
+  );
+};
+
+export default Opacity;
