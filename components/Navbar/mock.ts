cd44c111439f24835da786c29abf7612eccac8aa
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-05-04 14:23:49 -0300

    feat: add Navbar mock

---
+++ components/Navbar/mock.ts
@@ -0,0 +1,34 @@
+import { NavBarIconProps } from "./NavBarIcon";
+
+export const navbarIconMock = [
+  {
+    href: '/',
+    altName: 'Home Icon',
+    source: '/images/homeLogo.svg',
+  },
+  {
+    href: '/work',
+    altName: 'Work Icon',
+    source: '/images/workLogo.svg',
+  },
+  {
+    href: '/about',
+    altName: 'About Icon',
+    source: '/images/aboutLogo.svg',
+  },
+  {
+    href: 'https://github.com/PetrusBorges',
+    altName: 'Github Icon',
+    source: '/images/githubLogo.svg',
+  },
+  {
+    href: 'https://www.linkedin.com/in/petrusborgesmachado/',
+    altName: 'Linkedin Icon',
+    source: '/images/linkedinLogo.svg',
+  },
+  {
+    href: 'mailto:contatopetrusborges@gmail.com',
+    altName: 'Email Icon',
+    source: '/images/emailLogo.svg',
+  },
+] as NavBarIconProps[]
