9a1ae9b74b758edcfe1fb5283e87bcac99d7e34c
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-17 22:48:08 -0300

    feat: add page Test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function Test() {
+  return (
+    <section
+      className='flex items-center justify-center h-full'
+    >
+      <p className='text-red-500'>test</p>
+    </section>
+  )
+}
