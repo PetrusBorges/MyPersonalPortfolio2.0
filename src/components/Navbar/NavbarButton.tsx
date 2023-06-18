4380b7b3039abfbf843da1314c78ef0a4f46e43b
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-18 08:17:11 -0300

    feat: add props on NavbarButton

--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -9,11 +9,15 @@ import Image from 'next/image';
 interface NavbarButtonProps {
   src: string;
   href: string;
+  isButton?: boolean;
+  isAnchor?: boolean;
 }
 
 const NavbarButton: FC<NavbarButtonProps> = ({
   src,
-  href
+  href,
+  isButton = false,
+  isAnchor = false
 }) => {
   const router = useRouter()
   const pathname = usePathname()
@@ -25,17 +29,35 @@ const NavbarButton: FC<NavbarButtonProps> = ({
   }
 
   return (
-    <button
-      onClick={handleNavigate}
-      className={`flex items-center justify-center rounded-full border-t-2 border-neutral-700 w-12 h-12 duration-500 hover:w-20 hover:h-20 ${pathname === href ? 'bg-neutral-400 border-neutral-300' : 'bg-neutral-800'}`}
-    >
-      <Image 
-        src={src}
-        alt='ex'
-        width={26}
-        height={26}
-      />
-    </button>
+    <>
+      {isButton && (
+        <button
+        onClick={handleNavigate}
+        className={`flex items-center justify-center rounded-full border-t-2 border-neutral-700 w-12 h-12 duration-500 hover:w-20 hover:h-20 ${pathname === href ? 'bg-neutral-400 border-neutral-200' : 'bg-neutral-800'}`}
+      >
+        <Image 
+          src={src}
+          alt='ex'
+          width={26}
+          height={26}
+        />
+      </button>
+      )}
+      {isAnchor && (
+        <a
+          target='_blank'
+          href={href}
+          className={`flex items-center justify-center rounded-full border-t-2 border-neutral-700 w-12 h-12 duration-500 hover:w-20 hover:h-20 ${pathname === href ? 'bg-neutral-400 border-neutral-200' : 'bg-neutral-800'}`}
+        >
+          <Image 
+            src={src}
+            alt='navbarIcon'
+            width={26}
+            height={26}
+          />
+        </a>
+      )}
+    </>
   )
 }
 
