a8721a7df2112bc49c4b2227f9a8ce1ea2ddc4f4
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-18 08:17:36 -0300

    fix: styles on Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,31 +3,43 @@ import NavbarButton from "./NavbarButton"
 const Navbar = () => {
   return (
     <nav
-      className='border-2 backdrop-blur bg-white/5 border-neutral-800 flex items-center justify-center gap-1 p-2 rounded-full absolute bottom-5 duration-500 hover:border-transparent'
+      className='shadow-2xl border-2 border-neutral-800 backdrop-blur bg-white/5 flex items-center justify-center gap-1 p-2 rounded-full absolute bottom-5 duration-500 hover:border-transparent'
     >
       <NavbarButton 
+        isButton
         src="../assets/icons/homeIcon.svg"
         href='/'
       />
       <NavbarButton 
+        isButton
         src="../assets/icons/projectIcon.svg"
         href='/test'
       />
       <NavbarButton 
+        isButton
         src="../assets/icons/photoIcon.svg"
         href='/photos'
       />
+      <div className="w-[1px] h-10 bg-neutral-800 mx-2" />
       <NavbarButton 
-        src="../assets/icons/twitterIcon.svg"
-        href='/a'
-      />
-      <NavbarButton 
+        isButton
         src="../assets/icons/mailIcon.svg"
-        href='/v'
+        href='mailto:contatopetrusborges@gmail.com'
       />
       <NavbarButton 
+        isAnchor
+        src="../assets/icons/linkedinIcon.svg"
+        href='https://www.linkedin.com/in/petrusborgesmachado/'
+      />
+      <NavbarButton 
+        isAnchor
+        src="../assets/icons/twitterIcon.svg"
+        href='https://twitter.com/PetrusBorges_'
+      />
+      <NavbarButton 
+        isAnchor
         src="../assets/icons/gitIcon.svg"
-        href='/b'
+        href='https://github.com/PetrusBorges'
       />
     </nav>
   )
