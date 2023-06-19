7bb235ede2197c981f382b37ada5b45ca18ab1dc
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-19 19:46:47 -0300

    feat: creat mock and add ProjectCard on projects page

---
+++ src/app/projects/mock.ts
@@ -0,0 +1,31 @@
+import { ProjectInfoProps } from "@/@types/projectInfo";
+
+export const projectsMock = [
+  {
+    id: 1,
+    year: '2022',
+    title: 'string',
+    subtitle: 'string',
+    description: 'stringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstringstring',
+    href: '/assets/images/asd.jpg',
+    githubLink: 'https://tailwindcss.com/docs/customizing-colors',
+  },
+  {
+    id: 2,
+    year: '2022',
+    title: 'string',
+    subtitle: 'string',
+    description: 'string',
+    href: '/assets/images/asd.jpg',
+    githubLink: 'https://tailwindcss.com/docs/customizing-colors',
+  },
+  {
+    id: 3,
+    year: '2022',
+    title: 'string',
+    subtitle: 'string',
+    description: 'string',
+    href: '/assets/images/asd.jpg',
+    githubLink: 'https://tailwindcss.com/docs/customizing-colors',
+  }
+] as ProjectInfoProps[]
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,42 +1,19 @@
 'use client'
 
 import ProjectCard from "@/components/ProjectCard";
+import { projectsMock } from "./mock";
 
 export default function Projects() {
   return (
     <section
-      className="flex flex-col items-center justify-center gap-2 w-full lg:max-w-3xl px-5 lg:px-0"
+      className="flex flex-col items-center justify-center gap-1 w-full lg:max-w-3xl px-5 lg:px-0"
     >
-      <ProjectCard 
-        onClick={() => {}}
-        titile="Project"
-        subtitle="text"
-        year="2021"
-      />
-      <ProjectCard 
-        onClick={() => {}}
-        titile="Project"
-        subtitle="text"
-        year="2021"
-      />
-      <ProjectCard 
-        onClick={() => {}}
-        titile="Project"
-        subtitle="text"
-        year="2021"
-      />
-      <ProjectCard 
-        onClick={() => {}}
-        titile="Project"
-        subtitle="text"
-        year="2021"
-      />
-      <ProjectCard 
-        onClick={() => {}}
-        titile="Project"
-        subtitle="text"
-        year="2021"
-      />
+      {projectsMock.map((project) => (
+        <ProjectCard 
+          key={project.id}
+          projectInfo={project}
+        />
+      ))}
     </section>
   )
 }
