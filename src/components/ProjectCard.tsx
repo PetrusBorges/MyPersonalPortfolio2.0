36eff3004de7d0d5fb6c160a5bd9bf11bca590ad
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-19 19:45:53 -0300

    fix: styles on ProjectCard component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,28 +1,77 @@
-import { FC } from 'react'
+'use client'
 
+import { FC, useState } from 'react'
+
+import Image from 'next/image';
+
+import { ProjectInfoProps } from '@/@types/projectInfo';
 interface ProjectCardProps {
-  year: string;
-  titile: string;
-  subtitle: string;
-  onClick: () => void;
+  projectInfo: ProjectInfoProps;
 }
 
 const ProjectCard: FC<ProjectCardProps> = ({
-  year,
-  titile,
-  subtitle,
-  onClick
+  projectInfo
 }) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+
   return (
-    <button 
-      onClick={onClick}
-      className='flex items-center justify-start transition-all duration-500 hover:bg-neutral-800 w-full px-2 py-3 rounded-xl gap-2'
-    >
-      {titile}
-      <span className='text-gray-500 text-xs'>{subtitle}</span>
-      <div className='w-full h-[2px] bg-neutral-700' />
-      <span className='text-gray-500 text-xs'>{year}</span>
-    </button>
+    <>
+      <button 
+        onClick={() => setIsVisible((prevState) => !prevState)}
+        className='flex items-center justify-start transition-all duration-500 hover:bg-neutral-800 w-full px-2 py-3 rounded-xl gap-2'
+      >
+        {projectInfo.title}
+        <span className='text-gray-500 text-xs'>{projectInfo.subtitle}</span>
+        <div className='w-full h-[2px] bg-neutral-700' />
+        <span className='text-gray-500 text-xs'>{projectInfo.year}</span>
+      </button>
+
+      <div 
+        className={`rounded-lg w-full bg-neutral-800 transition-all duration-500 flex flex-col gap-2 items-center justify-start 
+        ${
+          isVisible ? 'visible opacity-100 h-96' : 'invisible opacity-0 h-0'
+        }`}
+      >
+        <div className='w-full h-36'>
+          <Image 
+            src={projectInfo.href}
+            alt='ProjectImage'
+            width={500}
+            height={500}
+            className='w-full h-full object-cover rounded-t-lg'
+          />
+        </div>
+
+        <p className='h-full w-full break-words px-3'>
+          {projectInfo.description}
+        </p>
+
+        <div className='w-full flex items-center justify-end gap-3 pr-2 py-2 border-t-2 border-[#363636]'>
+          <a
+            target='_blank'
+            href={projectInfo.githubLink}
+          >
+            <Image 
+              src='/assets/icons/linkIcon.svg'
+              alt='ProjectImage'
+              width={26}
+              height={26}
+            />
+          </a>
+          <a
+            target='_blank'
+            href={projectInfo.githubLink}
+          >
+            <Image 
+              src='/assets/icons/gitIcon.svg'
+              alt='ProjectImage'
+              width={26}
+              height={26}
+            />
+          </a>
+        </div>
+      </div>
+    </>
   )
 }
 
