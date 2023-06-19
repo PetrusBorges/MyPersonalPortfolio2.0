5c7b7a3ca245a26eae3971bbe6943a169240a579
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-06-19 09:59:47 -0300

    fix: styles ErrorMessage component

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,26 +1,21 @@
 'use client'
 
-import { FC } from 'react'
 import { useRouter } from 'next/navigation'
 
 import Image from 'next/image'
 import Button from './Button'
 
-interface ErrorMessageProps {
-  
-}
-
-const ErrorMessage: FC<ErrorMessageProps> = ({}) => {
+const ErrorMessage = () => {
   const router = useRouter()
 
   return (
-    <div className='flex items-center flex-col justify-start bg-neutral-900 w-96 h-96 rounded-xl border-2 border-gray-800'>
+    <div className='flex items-center flex-col justify-start bg-neutral-700 w-96 h-96 rounded-xl border-2 border-neutral-800 shadow-2xl shadow-neutral-900'>
       <div className='bg-neutral-800 w-full flex items-center justify-center rounded-t-lg h-6 relative'>
         Hello!
         <div className='absolute top-[5px] left-3 flex items-center justify-center gap-2'>
-          <div className='bg-neutral-600 rounded-full w-3 h-3' />
-          <div className='bg-neutral-600 rounded-full w-3 h-3' />
-          <div className='bg-neutral-600 rounded-full w-3 h-3' />
+          <div className='bg-neutral-700 rounded-full w-3 h-3' />
+          <div className='bg-neutral-700 rounded-full w-3 h-3' />
+          <div className='bg-neutral-700 rounded-full w-3 h-3' />
         </div>
       </div>
 
@@ -30,8 +25,8 @@ const ErrorMessage: FC<ErrorMessageProps> = ({}) => {
         <Image 
           src='/assets/icons/errorIcon.svg'
           alt='ErrorIcon'
-          width={98}
-          height={98}
+          width={88}
+          height={88}
         />
         <p>This page is under maintenance, please return to the main page!</p>
 
